bb8fd397dd504dee91d0de8faa78af11b76a58a5
Ky <kylesmith97@outlook.com>
2026-03-17 15:12:31 +0000

    homepage updates, first release card fixed, typography updates.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,23 +6,20 @@ function Home() {
     <div className="page-container">
       <section className="hero">
         <Heading level="1">Producer</Heading>
-        <Heading level="2">Audio Engineer</Heading>
-        <Heading level="3">Songwriter</Heading>
+        <Heading level="1">Audio Engineer</Heading>
+        <Heading level="1">Songwriter</Heading>
         <Text>lo-fi / ambient project</Text>
       </section>
 
       <section className="featured">
         <Heading level="2">Latest Release</Heading>
+        <Text level="5">Distance (album)</Text>
         <div className="featured-content">
           <img
-            src="/album-cover.jpg"
-            alt="Latest Album"
+            src="/atlis-distance-art.jpg"
+            alt="Distance Album"
             className="featured-image"
           />
-          <div className="featured-info">
-            <Heading level="3">Album Title</Heading>
-            <Text>Released: 2026</Text>
-          </div>
         </div>
       </section>
     </div>
